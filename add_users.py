5732b40d9695fc9bcbe537aac86b773df488539d
MaheshBharadwaj <maheshbharadwaj101@gmail.com>
2021-02-06 10:22:55 +0530

    minor commit

--- add_users.py
+++ add_users.py
@@ -1,20 +1,22 @@
 import sqlite3
 
-conn = sqlite3.connect('db.sqlite')
+conn = sqlite3.connect("db.sqlite")
 
-view_all = int(input('View all records 1/0: '))
+view_all = int(input("View all records 1/0: "))
 if view_all == 1:
-    cursor = conn.execute("SELECT id, name, email from USER")
+    cursor = conn.execute("SELECT id, name, email from user")
     for row in cursor:
         print("ID = ", row[0])
         print("NAME = ", row[1])
         print("EMAIL = ", row[2])
-        print('--------------------')
+        print("--------------------")
 
 
-add_user = int(input('Add user 1/0: '))
+add_user = int(input("Add user 1/0: "))
 if add_user == 1:
-    print('adding user')
-    out = conn.execute("INSERT INTO USER (ID,NAME,EMAIL) \
-      VALUES ('ADMIN3', 'Pritham', 'pritham123@gmail.com')")
+    print("adding user")
+    out = conn.execute(
+        "INSERT INTO USER (ID,NAME,EMAIL) \
+      VALUES ('ADMIN4', 'Pritham', 'pritham123@gmail.com')"
+    )
     print(out)
--- app.py
+++ app.py
@@ -11,14 +11,14 @@ from flask_login import login_user, logout_user, login_required, current_user
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__, static_folder=ROOT_DIR + "/static/")
-app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config["SECRET_KEY"] = "9OLWxND4o83j4K4iuopO"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
 
 db = SQLAlchemy()
 db.init_app(app)
 
 login_manager = LoginManager()
-login_manager.login_view = 'login'
+login_manager.login_view = "login"
 login_manager.init_app(app)
 
 
@@ -33,13 +33,18 @@ class User(UserMixin, db.Model):
 db.create_all(app=app)
 
 try:
-    new_user = User(id='ADMIN2', email='maheshbharadwaj18099@cse.ssn.edu.in',
-                    name='Mahesh', password=generate_password_hash('admin@ssn', method='sha256'))
+    new_user = User(
+        id="ADMIN3",
+        email="maheshbharadwaj18199@cse.ssn.edu.in",
+        name="Pritham",
+        password=generate_password_hash("admin@ssn", method="sha256"),
+    )
 
     with app.app_context():
         # add the new user to the database
         db.session.add(new_user)
         db.session.commit()
+        print("added user")
 except:
     pass
 
@@ -50,37 +55,37 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 
-@app.route('/login')
+@app.route("/login")
 def login():
-    return render_template('login.html')
+    return render_template("login.html")
 
 
-@app.route('/login', methods=['POST'])
+@app.route("/login", methods=["POST"])
 def login_post():
-    regid = request.form.get('regid')
-    password = request.form.get('password')
+    regid = request.form.get("regid")
+    password = request.form.get("password")
     remember = True  # if request.form.get('remember') else False
-    print('regid: %s\tpass: %s' % (regid, password))
+    print("regid: %s\tpass: %s" % (regid, password))
     user = User.query.filter_by(id=regid).first()
 
     # check if user actually exists
     # take the user supplied password, hash it, and compare it to the hashed password in database
     if not user or not check_password_hash(user.password, password):
-        flash('Please check your login details and try again.')
+        flash("Please check your login details and try again.")
         # if user doesn't exist or password is wrong, reload the page
-        return redirect(url_for('login'))
+        return redirect(url_for("login"))
 
     # if the above check passes, then we know the user has the right credentials
     login_user(user, remember=remember)
-    return redirect(url_for('dashboard'))
+    return redirect(url_for("dashboard"))
 
 
-@app.route('/')
-@app.route('/dashboard')
+@app.route("/")
+@app.route("/dashboard")
 @login_required
 def dashboard():
-    return render_template('dashboard.html', name=current_user.name)
+    return render_template("dashboard.html", name=current_user.name)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
