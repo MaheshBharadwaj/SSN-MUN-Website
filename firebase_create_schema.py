323fbf07c8814dae133a04a4a1483785ee6b4949
Mahesh Bharadwaj K <maheshbharadwaj101@gmail.com>
2021-11-10 21:58:44 +0530

    comment out dangerous lines in create_schema.py

--- firebase_create_schema.py
+++ firebase_create_schema.py
@@ -14,22 +14,23 @@ def main():
     # committees = ['HR']
 
     for committee in committees:
-        # for i in range(1, 800):
-        #     idx = str(i)
-        #     while len(idx) < 3:
-        #         idx = "0" + idx
+        for i in range(1, 800):
+            idx = str(i)
+            while len(idx) < 3:
+                idx = "0" + idx
 
         #     firestore_db.collection(committee).document(idx).set({'recv_count': 0, 'recv_messages': [], 'sent_count': 0, 'sent_messages': []})
-        firestore_db.collection(committee).document(committee + "EB").set({'recv_count': 0, 'recv_messages': [], 'sent_count': 0, 'sent_messages': []})
+        # firestore_db.collection(committee).document(u"EB").set({'recv_count': 0, 'recv_messages': [], 'sent_count': 0, 'sent_messages': []})
     snapshots = list(firestore_db.collection(u"HR").get())
     for snapshot in snapshots:
         print(snapshot.to_dict())
 
 
 if __name__ == "__main__":
-    print('This script deletes and resets all messages. Do you want to proceed 1/0: ', end='')
-    opt = int(input())
-    if opt == 1:
+    print('This script deletes and resets all messages. Do you want to proceed "Yes, I am sure" / "No": ', end='')
+    opt = input()
+    if opt == "Yes, I am sure":
         main()
     else:
+        print('Aborting....')
         pass
